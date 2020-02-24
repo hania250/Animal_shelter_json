10443aeac5bb02e446a2ad7593f9ececee87cf73
Hanna Bujna <hania250@gmail.com>
2020-02-24 14:42:39 +0100

    main  addition

---
+++ iofile.py
@@ -0,0 +1,54 @@
+import json
+import os
+
+class IOAnimals:
+
+    def __init__(self, dirname):
+        self.dirname = dirname
+
+    def get_filename(self, animal):
+        return self.dirname + '/' + animal
+
+    def load_animal(self, filename):
+        with open(self.get_filename(filename), 'r') as f:
+            d = json.load(f)
+            return d
+
+    def load_animals(self):
+        filenames = os.listdir(self.dirname + '/')
+        print(filenames)
+        animals = []
+        for filename in filenames:
+            d = self.load_animal(filename)
+            animals.append(d)
+        return animals
+
+
+    def save_animal(self, animal):
+        title = animal['Name']
+        title = title.lower()
+        title = title.replace(' ', '_')
+        print(title)
+
+        with open(self.get_filename(title) + '.json', 'w') as f:
+            json.dump(animal, f)
+
+def build_animal(name,
+                    date,
+                    condition,
+                    vaccination,
+                    notes=''):
+    d = {
+        "Name": name,
+        "Date": date,
+        "Condition": condition,
+        "Vaccination": vaccination,
+        "Notes": notes
+    }
+    return d
+
+if __name__=='__main__':
+    a = IOAnimals('animals')
+    item = build_animal('dog', '2019', 'ok', 'nie')
+    a.save_animal(item)
+
