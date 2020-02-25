7648ff7987ae73a799235712d5561d4d5a8a8634
Hanna Bujna <hania250@gmail.com>
2020-02-25 14:14:10 +0100

    functions: modify, find

--- organizer.py
+++ organizer.py
@@ -11,11 +11,26 @@ class AnimalOrganizer:
 
         self.io.save_animal(animal)
 
-    def modify_animal(self):
-        pass
+    def find_animal(self, arg, value):
+        for animal in self.animals:
+            if animal[arg] == value:
+                return animal
+        raise Exception(" pozycja nie istnieje! ")
+
+
+    def modify_animal(self, arg, value, set_arg, set_value):
+        animal = self.find_animal(arg, value)
+        animal[set_arg] = set_value
+        self.io.save_animal(animal)
 
     def get_all_animals(self):
         return self.animals
 
 b = AnimalOrganizer('animals')
-print(b.get_all_animals())
+
+
+#b.add_animal('a', '2011', 'ok', 'nie')
+#b.add_animal('c', '2011', 'ok', 'nie')
+#print(b.get_all_animals())
+
+b.modify_animal('Name', 'c', 'Vaccination', 'nie')
